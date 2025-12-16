ee6d7ee3933c189d7cb2483e94a5dc1542989f57
mip5 <54771155+mip5@users.noreply.github.com>
2025-12-16 14:21:47 +0000

    commit

---
+++ main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a month of the year: ");
+        String month = sc.nextLine();
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        sc.nextLine(); // clear buffer
+
+        System.out.print("Enter a verb: ");
+        String verb3 = sc.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String adjective = sc.nextLine();
+
+        System.out.print("Enter a noun: ");
+        String noun = sc.nextLine();
+
+        System.out.print("Enter a plural noun: ");
+        String pluralNoun = sc.nextLine();
+
+        System.out.print("Enter a past tense verb: ");
+        String pastTenseVerb = sc.nextLine();
+
+        System.out.println();
+        System.out.println(\\t
+            "On " + month + ", a " + number + " " + noun +
+            " decided to " + verb3 + " through the " + adjective + " " + noun + ". " +
+            "Along the way, it found " + number + " " + pluralNoun +
+            " that were extremely " + adjective + ". " +
+            "Without thinking, it " + pastTenseVerb + " the " + noun +
+            " and ran away laughing."
+        );
+    }
+}
